572b23c58269a2c10360975a35dabed824875874
Osmond Oscar <osmond.oranagwa@andela.com>
2016-09-02 01:13:06 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'conversations/web_mobile'
+      post 'conversations/web_mobile'
+      get "/webhook", to: "webhook#verify"
+      post "/webhook", to: "webhook#handler"
     end
   end
 end
