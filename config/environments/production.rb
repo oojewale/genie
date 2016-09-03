238f93673ec15450712b844a095b1c3407ae40d2
Osmond Oscar <osmond.oranagwa@andela.com>
2016-09-03 14:36:04 +0100

    update send_grid params

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
-    domain: "heroku.com",
+    domain: "bluemix.net",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
